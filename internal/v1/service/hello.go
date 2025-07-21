f9772d797f79bc1cda1bd10fa11cdad1ac05bcd0
Naweeparb <naweeparbw@borneothai.com>
2025-07-21 17:01:59 +0700

    add gitignore

--- internal/v1/service/hello.go
+++
@@ -1,7 +0,0 @@
-// internal\v1\service\hello.go
-
-package service
-
-func HelloService() string {
-    return "hello from v1"
-}
--- internal/v2/service/hello.go
+++
@@ -1,5 +0,0 @@
-package service
-
-func HelloService() string {
-    return "hello from v2 (new logic)"
-}
